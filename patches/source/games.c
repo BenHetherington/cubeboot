10a43fde14cb2f6d4caf22d16b7848a439f35f09
Ben Hetherington <dev@ben-h.uk>
2025-07-07 20:58:00 +0100

    Use sRGB for icon textures, and resolve most TODOs
    The disc/FlippyDrive/L/R icons now look significantly better, as they’re displayed as sRGB as intended (rather than being interpreted as linear colours).
    
    Other than that, this re-adds support for post-boot delays, moves `cube_menu_alpha` to a shared header, and removes some TODOs from disc-loading that aren’t going to be done, or have already been done.

--- patches/source/games.c
+++ patches/source/games.c
@@ -1022,7 +1022,6 @@ void *gm_disc_thread_worker(void *param) {
             }
 
             finished_reading_disc = true;
-            // TODO: Spin down the disc?
             continue;
         }
 
@@ -1041,7 +1040,6 @@ void *gm_disc_thread_worker(void *param) {
         if (!game_info.valid) {
             disc_read_state = STATE_READ_ERROR;
             finished_reading_disc = true;
-            // TODO: Spin down the disc?
             continue;
         }
 
@@ -1186,6 +1184,5 @@ void gm_deinit_thread() {
         OSReport("File enum done\n");
         OSUnlockMutex(game_enum_mutex);
     }
-    // TODO: Handle the disc thread!
 }
 
